1e9b55ba05639702bf392d2c47fd8997d3b663e5
mostafijur <mrmoon025@gmail.com>
2025-06-01 13:09:13 +0600

    [feat] Added api response.

--- kpi/bootstrap/app.php
+++ kpi/bootstrap/app.php
@@ -3,16 +3,38 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->renderable(function (ValidationException $e, $request) {
+            if ($request->is('api/*') || $request->wantsJson()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation failed',
+                    'errors' => $e->errors(),
+                    'data' => '',
+                ], 422);
+            }
+        });
+
+        $exceptions->renderable(function (\Throwable $e, $request) {
+            if ($request->is('api/*') || $request->wantsJson()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => $e->getMessage() ?: 'Server Error',
+                    'errors' => '',
+                    'data' => '',
+                ], 500);
+            }
+        });
     })->create();
