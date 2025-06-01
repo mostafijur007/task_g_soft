99ca7a7fe2316a04481f99c45a43af182969d875
mostafijur <mrmoon025@gmail.com>
2025-06-01 14:58:23 +0600

    [refactor] Customer model.

--- kpi/app/Models/Customer.php
+++ kpi/app/Models/Customer.php
@@ -4,12 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'code',
+        'name',
+        'email',
+        'phone',
+        'address'
+    ];
+
+    /**
+     * Get the products associated with the customer.
+     */
     public function products()
     {
         return $this->belongsToMany(Product::class, 'customer_product');
