d816b55deacbb66542771658813555cf9d54fff8
mostafijur <mrmoon025@gmail.com>
2025-06-01 13:06:35 +0600

    [feat] Added KPI model, migration repository, interface, service, factory, controller and seeder.

---
+++ kpi/app/Models/Supplier.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    /** @use HasFactory<\Database\Factories\SupplierFactory> */
+    use HasFactory;
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_supplier');
+    }
+}
---
+++ kpi/app/Repositories/SupplierRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Supplier;
+use App\Repositories\Interfaces\SupplierRepositoryInterface;
+
+class SupplierRepository implements SupplierRepositoryInterface
+{
+    public function all()
+    {
+        return Supplier::all();
+    }
+
+    public function find($id)
+    {
+        return Supplier::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Supplier::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->update($data);
+        return $supplier;
+    }
+
+    public function delete($id)
+    {
+        return Supplier::findOrFail($id)->delete();
+    }
+}
