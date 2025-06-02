784edfa00835183bdbc9e8f7e267075998064925
Mostafijur <mrmoon025@gmail.com>
2025-06-02 21:09:04 +0600

    [feat] Added customer factory & seeder.

--- kpi/database/factories/CustomerFactory.php
+++ kpi/database/factories/CustomerFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,26 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
+        static $codeNumber = 1;
         return [
-            //
+            'code' => 'CUS-' . str_pad($codeNumber++, 4, '0', STR_PAD_LEFT),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->optional()->phoneNumber(),
+            'address' => $this->faker->optional()->address(),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Customer $customer) {
+            $productIds = Product::inRandomOrder()->take(rand(1, 5))->pluck('id');
+
+            if ($productIds->isEmpty()) {
+                $productIds = Product::factory()->count(5)->create()->pluck('id');
+            }
+
+            $customer->products()->attach($productIds);
+        });
     }
 }
